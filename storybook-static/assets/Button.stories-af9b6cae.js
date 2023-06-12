f0482680cccb12764c9e03c68ff2dff1246281ac
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2023-06-12 11:58:45 +0300

    adding static storybook

---
+++ storybook-static/assets/Button.stories-af9b6cae.js
@@ -0,0 +1,17 @@
+import{B as e,j as n}from"./Button-f24a4c64.js";import"./index-c013ead5.js";import"./_commonjsHelpers-725317a4.js";const x={component:e,title:"Button",tags:["autodocs"]},r={args:{variant:"primary",onClick:()=>{}}},a={args:{variant:"secondary",onClick:()=>{}}},o={render:()=>n.jsxs("div",{children:[n.jsx(e,{variant:"primary",onClick:()=>{}}),n.jsx(e,{variant:"secondary",onClick:()=>{}})]})};var s,t,c,i,m;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    variant: "primary",
+    onClick: () => {}
+  }
+}`,...(c=(t=r.parameters)==null?void 0:t.docs)==null?void 0:c.source},description:{story:"something about Primary component",...(m=(i=r.parameters)==null?void 0:i.docs)==null?void 0:m.description}}};var p,d,u;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    variant: "secondary",
+    onClick: () => {}
+  }
+}`,...(u=(d=a.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var l,y,v;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: () => <div>
+      <Button variant="primary" onClick={() => {}} />
+      <Button variant="secondary" onClick={() => {}} />
+    </div>
+}`,...(v=(y=o.parameters)==null?void 0:y.docs)==null?void 0:v.source}}};const j=["Primary","Secondary","Group"];export{o as Group,r as Primary,a as Secondary,j as __namedExportsOrder,x as default};
+//# sourceMappingURL=Button.stories-af9b6cae.js.map
---
+++ storybook-static/assets/preview-db740236.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-64d9441d.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-16353741.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-16353741.js","./iframe-64d9441d.js","./index-c013ead5.js","./_commonjsHelpers-725317a4.js","./_getPrototype-dd8f0158.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-db740236.js.map
