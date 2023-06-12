15cdd7d904ff62f2ab6939910757e9ae16d974b3
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2023-06-12 12:16:44 +0300

    test

--- src/Button.tsx
+++ src/Button.tsx
@@ -5,29 +5,29 @@ type Props = { variant?: "primary" | "secondary" };
 
 const StyledButton = styled.button<Props>`
     background: ${(props) =>
-        props.variant === "primary" ? "#bf4f74" : "transparent"};
+        props.variant === "primary" ? "#5EBF4F" : "transparent"};
     border-radius: 3px;
     border: ${(props) =>
-        props.variant === "primary" ? "2px solid #bf4f74" : "2px solid #000"};
+        props.variant === "primary" ? "2px solid #5EBF4F" : "2px solid #000"};
     color: ${(props) => (props.variant === "primary" ? "white" : "#000")};
     margin: 0 1em;
     padding: 1em 2em;
 
     &:hover {
         background: ${(props) => (props.variant === "primary" ? "#fff" : "#000")};
-        color: ${(props) => (props.variant === "primary" ? "#bf4f74" : "#fff")};
+        color: ${(props) => (props.variant === "primary" ? "#5EBF4F" : "#fff")};
         cursor: pointer;
     }
 
     &:focus {
-        background: ${(props) => (props.variant === "primary" ? "#f5f5f5" : "#828282")};
-        color: ${(props) => (props.variant === "primary" ? "#bf4f74" : "#fff")};
+        background: ${(props) => (props.variant === "primary" ? "#f5f5f5" : "#d9d9d9")};
+        color: ${(props) => (props.variant === "primary" ? "#5EBF4F" : "#fff")};
         outline: none;
     }
 
     &:active {
         background: ${(props) => (props.variant === "primary" ? "#f5f5f5" : "#000")};
-        color: ${(props) => (props.variant === "primary" ? "#bf4f74" : "#fff")};
+        color: ${(props) => (props.variant === "primary" ? "#5EBF4F" : "#fff")};
     }
 `;
 
