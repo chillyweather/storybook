9e1125c624b5ba0b47ea9d220faab1097c68be5d
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2023-06-12 08:49:31 +0300

    storybook added

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 100,
     },
   },
 });
